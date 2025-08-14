2cf2413ab83e6c92863562244a469284014ea6f8
shabeeh <shabeeh573@gmail.com>
2025-08-14 21:52:02 +0530

    completed layout and styling upto banner

---
+++ main.js
@@ -0,0 +1,10 @@
+// const navButtons = document.querySelectorAll(".nav-links button");
+
+// navButtons.forEach((btn) => {
+
+//   btn.addEventListener("click", () => {
+//     navButtons.forEach((b) => b.classList.remove("active"));
+//   });
+
+//   btn.classList.add("active");
+// });
