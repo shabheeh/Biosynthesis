f43ebb812069fad1d836492f98eeb1d2580b8a7e
shabeeh <shabeeh573@gmail.com>
2025-08-16 21:45:27 +0530

    added responsivenes for mobile and tab screens

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
-// const navButtons = document.querySelectorAll(".nav-links button");
+const navLinks = document.querySelectorAll(".nav-links .nav-link");
 
-// navButtons.forEach((btn) => {
+navLinks.forEach((link) => {
+  link.addEventListener("click", function (e) {
+    navLinks.forEach((nav) => nav.classList.remove("active"));
 
-//   btn.addEventListener("click", () => {
-//     navButtons.forEach((b) => b.classList.remove("active"));
-//   });
+    this.classList.add("active");
+  });
+});
 
-//   btn.classList.add("active");
-// });
+const burgerMenu = document.getElementById("burgerMenu");
+const mobileNav = document.getElementById("mobileNav");
+const overlay = document.getElementById("overlay");
+
+function toggleMobileNav() {
+  burgerMenu.classList.toggle("active");
+  mobileNav.classList.toggle("active");
+  overlay.classList.toggle("active");
+
+  document.body.style.overflow = mobileNav.classList.contains("active")
+    ? "hidden"
+    : "auto";
+}
+
+burgerMenu.addEventListener("click", toggleMobileNav);
+
+overlay.addEventListener("click", toggleMobileNav);
